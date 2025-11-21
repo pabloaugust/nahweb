3631a38a5dadf514e7833f39ace9298dfca1870b
Pablo August <pabloaugustw@gmail.com>
2025-11-21 15:49:53 +0100

    init

--- config.js
+++ config.js
@@ -1,1 +1,5 @@
-window.API_URL = 'http://192.168.2.2:8000';
+// config.js
+// Nederlandse opmerking: configuratie voor LM Studio API endpoint
+window.API_URL = 'http://192.168.2.2:1234';
+window.MODEL_NAME = 'geitje-7b-ultra'; // Nederlands taalmodel
+// config.js
